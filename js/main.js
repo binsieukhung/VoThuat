07842b157326c2570b7fa4cfb09234fe3e4a38d0
NgocMinh76 <xpreria12@gmail.com>
2017-03-27 16:20:27 +0700

    new

--- js/main.js
+++ js/main.js
@@ -36,13 +36,12 @@ $(document).ready(function(){
 	  autoHover:true,
 	  
 	});
-});
-	$('.list_notify').marquee({
+	
+	$('.list_notify').show().marquee({
 		//speed in milliseconds of the marquee
-		duration: 13000,
+		duration: 20000,
 		//gap in pixels between the tickers
-		gap: 0,
-
+		gap: 20,
 		//time in milliseconds before the marquee will start animating
 		delayBeforeStart: 0,
 		//'left' or 'right'
@@ -51,34 +50,7 @@ $(document).ready(function(){
 		duplicated: true,
 		pauseOnHover:1
 	});
-
-
-// // SLICK
-$(document).ready(function(){
- $('.slides').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  draggable: false,
-  arrows: true,
-  fade: true,
-  asNavFor: '.thumbnail',
-  dots:false,
-  // autoplay:true,
-  autoplaySpeed:1000,
 });
-$('.thumbnail').slick({
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  asNavFor: '.slides',
-  dots: false,
-  centerMode: false,
-  focusOnSelect: true,
-  draggable: true,
-
-	});
-});	
-
-
 $(function(){
 	  $('#scroll_news_hot').slimScroll({
 		  height: '430px',
---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$(window).scroll(function() {
+			if ($(this).scrollTop() != 0) {
+				$('.to_top').fadeIn();
+			} else {
+				$('.to_top').fadeOut();
+			}
+			});
+			$('.to_top').click(function() {
+				$('body,html').animate({
+				scrollTop: 0
+			}, 500);
+		});
+})
